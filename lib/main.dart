fd6cec4fcf7674cdef5cc638501f2865c22f0c56
Michael Kennecke <mkennecke@icloud.com>
2020-10-24 14:05:26 +0200

    Main UI with BottomNavigationBar, Header and InputsFields for entering new words

--- lib/main.dart
+++ lib/main.dart
@@ -24,6 +24,14 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,27 +39,59 @@ class _HomePageState extends State<HomePage> {
         title: Text("Easy Vocab"),
         actions: <Widget>[
           IconButton(
-            icon: const Icon(Icons.create),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const Icon(Icons.all_inbox),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const Icon(Icons.model_training),
-            onPressed: () {},
-          ),
-          IconButton(
-            icon: const Icon(Icons.emoji_events),
-            onPressed: () {},
-          ),
-          IconButton(
             icon: const Icon(Icons.settings),
             onPressed: () {},
+            tooltip: "Settings",
           ),
         ],
+      ),
+      body: Column(
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(
+              prefixIcon: Icon(Icons.translate),
+              hintText: "Enter vocab",
+            ),
+          ),
+          TextField(
+            decoration: InputDecoration(
+              //border: InputBorder.none,
+              prefixIcon: Icon(Icons.translate),
+              hintText: "Enter translation",
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: FloatingActionButton(
+              onPressed: () {
+                // add code
+              },
+              child: Icon(Icons.send),
+            ),
+          )
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.create),
+            label: "Add",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.model_training),
+            label: "Train",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.emoji_events),
+            label: "Test",
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+        iconSize: 30,
       ),
     );
   }
 }
+
+// Hier weitermachen: https://api.flutter.dev/flutter/material/BottomNavigationBar-class.html
