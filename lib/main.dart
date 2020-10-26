03e7f9d427e694956c4c47bdf6fb503126bb5b51
Michael Kennecke <mkennecke@icloud.com>
2020-10-26 15:55:50 +0100

    Added a callback function to add user input (word and translation of the word) to a ListView

--- lib/main.dart
+++ lib/main.dart
@@ -25,10 +25,17 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _selectedIndex = 0;
+  List<Translation> translations = [];
 
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
+    });
+  }
+
+  void newTranslation(String word, String wordTranslated) {
+    this.setState(() {
+      translations.add(new Translation(word, wordTranslated));
     });
   }
 
@@ -47,28 +54,8 @@ class _HomePageState extends State<HomePage> {
       ),
       body: Column(
         children: <Widget>[
-          TextField(
-            decoration: InputDecoration(
-              prefixIcon: Icon(Icons.translate),
-              hintText: "Enter vocab",
-            ),
-          ),
-          TextField(
-            decoration: InputDecoration(
-              //border: InputBorder.none,
-              prefixIcon: Icon(Icons.translate),
-              hintText: "Enter translation",
-            ),
-          ),
-          Padding(
-            padding: EdgeInsets.all(16.0),
-            child: FloatingActionButton(
-              onPressed: () {
-                // add code
-              },
-              child: Icon(Icons.send),
-            ),
-          )
+          TranslationInputWidget(this.newTranslation),
+          Expanded(child: TranslationList(this.translations)),
         ],
       ),
       bottomNavigationBar: BottomNavigationBar(
@@ -94,4 +81,105 @@ class _HomePageState extends State<HomePage> {
   }
 }
 
-// Hier weitermachen: https://api.flutter.dev/flutter/material/BottomNavigationBar-class.html
+class Box {
+  int id;
+  String name;
+  List<Translation> translations;
+  bool selected = false;
+}
+
+class Translation {
+  String word;
+  String wordTranslated;
+
+  Translation(this.word, this.wordTranslated);
+}
+
+class TranslationInputWidget extends StatefulWidget {
+  final Function(String, String) callback;
+
+  TranslationInputWidget(this.callback);
+
+  @override
+  _TranslationInputWidgetState createState() => _TranslationInputWidgetState();
+}
+
+class _TranslationInputWidgetState extends State<TranslationInputWidget> {
+  final wordController = TextEditingController();
+  final wordTranslatedController = TextEditingController();
+
+  void click() {
+    widget.callback(wordController.text, wordTranslatedController.text);
+    FocusScope.of(context).unfocus();
+    wordController.clear();
+    wordTranslatedController.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        TextField(
+          controller: this.wordController,
+          decoration: InputDecoration(
+            prefixIcon: Icon(Icons.translate),
+            hintText: "Enter vocab",
+          ),
+        ),
+        TextField(
+          controller: this.wordTranslatedController,
+          decoration: InputDecoration(
+            prefixIcon: Icon(Icons.translate),
+            hintText: "Enter translation",
+          ),
+        ),
+        Padding(
+          padding: EdgeInsets.all(16.0),
+          child: FloatingActionButton(
+            onPressed: this.click,
+            child: Icon(Icons.send),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class TranslationList extends StatefulWidget {
+  final List<Translation> listItems;
+
+  // constructor
+  TranslationList(this.listItems);
+
+  @override
+  _TranslationListState createState() => _TranslationListState();
+}
+
+class _TranslationListState extends State<TranslationList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
+      itemCount: this.widget.listItems.length,
+      itemBuilder: (BuildContext context, int index) {
+        var translation = this.widget.listItems[index];
+        return Container(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                translation.word,
+                style: TextStyle(fontSize: 20),
+              ),
+              Text(
+                translation.wordTranslated,
+                style: TextStyle(fontSize: 15),
+              ),
+            ],
+          ),
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) => const Divider(),
+    );
+  }
+}
